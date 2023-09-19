d05aaf48753098c6685921c60587c7c4bbb1c4a3
Misael <maq3009@hotmail.com>
2023-09-19 04:36:03 -0400

    Everything works.  Added Boiler App title

--- App.js
+++ App.js
@@ -1,13 +1,33 @@
 import React from "react"
-import { View, Text } from 'react-native'
+import { View, Text, SafeAreaView, StyleSheet } from 'react-native'
 
 const App = () => {
   return (
-    <View>
-      <Text>Current Weather</Text>
-    </View>
+    <SafeAreaView style={styles.wrapper}>
+      <View style={styles.container}>
+        <Text style={styles.text}>Boiler Room Inventory App</Text>
+      </View>
+    </SafeAreaView>
   )
 
 }
+
+const styles = StyleSheet.create({
+  wrapper: {
+    flex: 1
+  },
+  container: {
+    backgroundColor: 'lightblue',
+    flex: 1,
+    alignItems: 'center'
+  },
+  text: {
+    fontSize: 22,
+    fontWeight: 'bold',
+    color: "black",
+  },
+})
+
+
 
 export default App
