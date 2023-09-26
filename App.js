0156575bc6525450b9d754829ff36e4d0c558cf9
Misael <maq3009@hotmail.com>
2023-09-25 20:42:05 -0400

    5th or so commit, changed screen orientation in manifest to sensor

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
 import React from "react"
-import { View, Text, SafeAreaView, StyleSheet } from 'react-native'
+import { View, Text, Image, SafeAreaView, StyleSheet } from 'react-native'
 
 const App = () => {
+  let x = 1;
   return (
     <SafeAreaView style={styles.wrapper}>
       <View style={styles.container}>
         <Text style={styles.text}>Boiler Room Inventory App</Text>
+        <View style={styles.boilerContainer}>
+          <Image 
+            source={require('./assets/Boiler.png')}
+            style={{ height: 95, width: 300 }}
+          />
+        </View>
       </View>
     </SafeAreaView>
   )
@@ -14,18 +21,26 @@ const App = () => {
 
 const styles = StyleSheet.create({
   wrapper: {
-    flex: 1
+    flex: 1,
+    borderWidth: 3,
+    borderColor: "black",
+    borderRadius: 5,
   },
   container: {
     backgroundColor: 'lightblue',
     flex: 1,
     alignItems: 'center'
+    
   },
   text: {
-    fontSize: 22,
+    fontSize: 25,
     fontWeight: 'bold',
     color: "black",
+    paddingTop: 20,
   },
+  boilerContainer: {
+    paddingTop: 50,
+  }
 })
 
 
